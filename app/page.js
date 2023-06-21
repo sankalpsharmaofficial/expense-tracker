dc033793352831c0f194b40b16ea518c5bebb537
Sankalp Sharma <sankalp.sharma156@gmail.com>
2023-06-21 12:47:44 +0530

    fixed light mode theme

--- app/page.js
+++ app/page.js
@@ -99,13 +99,13 @@ export default function Home() {
 								key={id}
 								className="my-4 w-full flex justify-between bg-slate-950"
 							>
-								<div className="p-4 w-full flex justify-between">
+								<div className="p-4 w-full flex justify-between text-white">
 									<span className="capitalize">{item.name}</span>
 									<span>₹{item.price}</span>
 								</div>
 								<button
 									onClick={() => deleteItem(item.id)}
-									className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16"
+									className="ml-8 p-4 border-l-2 text-white border-slate-900 hover:bg-slate-900 w-16"
 								>
 									X
 								</button>
@@ -115,7 +115,7 @@ export default function Home() {
 					{items.length < 1 ? (
 						''
 					) : (
-						<div className="flex justify-between p-3">
+						<div className="flex justify-between p-3 text-white">
 							<span>Total</span>
 							<span>₹{total}</span>
 						</div>
